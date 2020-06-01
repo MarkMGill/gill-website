0df3357581a66911a8c982125964c35f2517c7cf
MarkMGill <40337791+MarkMGill@users.noreply.github.com>
2020-06-01 15:35:17 -0400

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+
+
+        // Get the current year for the copyright
+        $('#year').text(new Date().getFullYear());
+        
+        //Init Scrollspy
+        $('body').scrollspy({target: '#main-nav'});
+        
+        //add smooth scrolling
+        $('#main-nav a').on('click', function(e) {
+          //check for a hash value
+          if(this.hash !== '') {
+              //prevent default behavior
+              e.preventDefault();
+
+              //store hash
+              const hash = this.hash;
+
+              //animate smooth scroll
+              $('html, body').animate({
+                  scrollTop: $(hash).offset().top
+              }, 900, function() {
+                  //add hash to url after scroll
+                  window.location.hash = hash;
+              });
+            }
+        });
+
+        $('#home-section a').on('click', function(e) {
+            //check for a hash value
+            if(this.hash !== '') {
+                //prevent default behavior
+                e.preventDefault();
+  
+                //store hash
+                const hash = this.hash;
+  
+                //animate smooth scroll
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 900, function() {
+                    //add hash to url after scroll
+                    window.location.hash = hash;
+                });
+              }
+          });
